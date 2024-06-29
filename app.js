26fe07233e479f4c9586aa65fabb461a102ab5fc
LuisYepes601 <yepesluis006@gmail.com>
2024-06-29 13:28:30 -0500

    actualizaciónNumero2

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 
- let texto = document.querySelector("h1");
+let texto = document.querySelector("h1");
 texto.innerHTML = "Hora del desafio"
 
 //const button = document.getElementById('.button button_consola');
 
-function mostrarMensaje(){
+function mostrarMensaje() {
     console.log('El boton fue clicado')
+}
+
+function buttonPromp() {
+    var ciudad = prompt("Ingrese una ciudad de Brasil")
+    alert(`Estuve en ${ciudad} y me acordé de ti.`)
+}
+
+function buttonAlert() {
+    alert("Yo amo JS")
+}
+function suma() {
+    let num1 ;
+    let num2;
+
+    num1= parseInt(prompt("Ingrese el primer valor"));
+    num2= parseInt(prompt("Ingrese el segundo valor")) 
+
+    let suma =  parseInt (num1+num2);
+    alert("La suma de " +num1+ " + " + num2 + " es: "+ suma)
 }
