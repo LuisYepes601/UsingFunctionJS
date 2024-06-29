0cb81e06d1f24951c6870106ce89d0579348b088
LuisYepes601 <yepesluis006@gmail.com>
2024-06-29 12:24:50 -0500

    ActualizacionNumero1

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 
+ let texto = document.querySelector("h1");
+texto.innerHTML = "Hora del desafio"
+
+//const button = document.getElementById('.button button_consola');
+
+function mostrarMensaje(){
+    console.log('El boton fue clicado')
+}
